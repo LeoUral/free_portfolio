cdc5c8aa3c8d324121aaeb0b9c65c53fca078ee3
Leonid <leoural66@yandex.ru>
2020-12-21 22:37:10 +0500

    наполнение контентом

--- src/components/dataWork.js
+++ src/components/dataWork.js
@@ -1,11 +1,11 @@
 
 const DATA_WORK = [
-    { id: 1, url: "http://www.leo-photo.ru", text: "Сайт фотографа. Созданный с нуля. (HTML, SCSS, JS)", title: "Work one", urlImg: "/img/bg-3.jpg" },
-    { id: 2, url: "https://best-service.pro", text: "Сайт где производились правки контента, дизана. Менялись блоки, кнопки. (HTML, CSS, JS)", title: "Work two", urlImg: "/img/bg-4.jpg" },
-    { id: 3, url: "https://tech-professional24.ru/", text: "Сайт где производились правки контента, дизана. Менялись блоки, кнопки. Добавлялся контент. (HTML, CSS, JS)", title: "Work three", urlImg: "/img/bg-5.jpg" },
-    { id: 4, url: "https://service-tehniki.ru/", text: "Сайт был изготовлен по тех.заданию. Делался в группе с бэк-эндом.(HTML, SCSS, JS, PHP)", title: "Work four", urlImg: "/img/bg-6.jpg" },
+    { id: 1, url: "http://www.leo-photo.ru", text: "Сайт фотографа. Созданный с нуля. (HTML, SCSS, JS)", title: "leo-photo", urlImg: "/img/Screenshot_1.png" },
+    { id: 2, url: "https://best-service.pro", text: "Сайт где производились правки контента, дизана. Менялись блоки, кнопки. (HTML, CSS, JS)", title: "Best-service", urlImg: "/img/Screenshot_2.png" },
+    { id: 3, url: "https://tech-professional24.ru/", text: "Сайт где производились правки контента, дизана. Менялись блоки, кнопки. Добавлялся контент. (HTML, CSS, JS)", title: "tech-professional24", urlImg: "/img/Screenshot_3.png" },
+    { id: 4, url: "https://service-tehniki.ru/", text: "Сайт был изготовлен по тех.заданию. Делался в группе с бэк-эндом.(HTML, SCSS, JS, PHP)", title: "Техсервис мастер", urlImg: "/img/Screenshot_4.png" },
     { id: 5, url: "https://best-service.pro", text: "Сайт где производились правки контента, дизана. Менялись блоки, кнопки. (HTML, CSS, JS)", title: "Work two", urlImg: "/img/bg-1.jpg" },
-    { id: 6, url: "https://tech-professional24.ru/", text: "Сайт где производились правки контента, дизана. Менялись блоки, кнопки. Добавлялся контент. (HTML, CSS, JS)", title: "Work three", urlImg: "/img/bg-2.jpg" }
+    { id: 6, url: "https://github.com/LeoUral", text: "Ссылка на мой репозиторий.", title: "github.com/LeoUral", urlImg: "/img/Screenshot_6.png" }
 ];
 
 export default DATA_WORK
