2518aec8a08434458fe94f2d7e43ad7cdd6b4a95
Leonid <leoural66@yandex.ru>
2020-12-24 20:16:41 +0500

    add modal window with hook
    Добавлено модальное окно с слыками на соц.сети. Добавлена анимация. Управление модльным окном через хуки

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Jumbotron } from 'react-bootstrap';
 import '../style/contact.css';
-import icon_1 from '../image/instagram_socialnetwork_17393.png';
-import icon_2 from '../image/telegram_tile_logo_icon_169640.png';
-import icon_3 from '../image/vk_logo_icon_147212.png';
-import icon_4 from '../image/Git_icon-icons-blue.com_66557.png';
+import ModalWindow from './modalWindow';
+
 
 
 
 export default function Contact() {
+
+    const [show, setShow] = useState(false);
+
+    const handleShow = () => setShow(true);
+    const handleOffShow = () => setShow(false);
 
     return (
         <>
@@ -17,21 +20,12 @@ export default function Contact() {
                     <div className="contact_jumbo_block" >
                         <h2> Контакты </h2>
                         <p> Со мной можно связаться несколькими способами, быстрее всего написать. </p>
-                        <div className="contact_jumbo_block_links">
-                            <a href="#header" target="_blank" rel="noreferrer" className="contact_link" title="Напишите мне в Инстаграмм">
-                                <img src={icon_1} alt='icon' />
-                            </a>
-                            <a href="#header" target="_blank" rel="noreferrer" className="contact_link" title="Напишите мне в Телеграмм">
-                                <img src={icon_2} alt='icon' />
-                            </a>
-                            <a href="#header" target="_blank" rel="noreferrer" className="contact_link" title="Напишите мне в ВК">
-                                <img src={icon_3} alt='icon' />
-                            </a>
-                            <a href="#header" target="_blank" rel="noreferrer" className="contact_link" title="На GIT можно посмотреть мои работы">
-                                <img src={icon_4} alt='icon' />
-                            </a>
-                        </div>
-
+                        <ModalWindow show={show} offShow={handleOffShow} />
+                        <button className="btn_open" onClick={handleShow}>
+                            <div className="in_btn">
+                                <span>contacts</span>
+                            </div>
+                        </button>
                     </div>
                 </Jumbotron>
             </Container>
---
+++ src/components/modalWindow.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../style/modalWindow.css';
+import icon_1 from '../image/instagram_socialnetwork_17393.png';
+import icon_2 from '../image/telegram_tile_logo_icon_169640.png';
+import icon_3 from '../image/vk_logo_icon_147212.png';
+import icon_4 from '../image/Git_icon-icons-blue.com_66557.png';
+
+export default function ModalWindow(props) {
+
+    const handleClose = () => {
+        props.offShow();
+    }
+
+    return (
+        <>
+            <div className="modal_shadow" onClick={handleClose} style={{ display: props.show ? 'block' : 'none' }}></div>
+            <div className="modal_window" style={{ display: props.show ? 'block' : 'none' }}>
+                <h3>Modal Window</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore odio facilis minus nisi obcaecati ipsum numquam sunt, magni sint omnis nobis accusamus harum, velit ratione.</p>
+                <div className="contact_jumbo_block_links">
+                    <a href="#header" target="_blank" rel="noreferrer" className="contact_link" title="Напишите мне в Инстаграмм">
+                        <img src={icon_1} alt='icon' />
+                    </a>
+                    <a href="#header" target="_blank" rel="noreferrer" className="contact_link" title="Напишите мне в Телеграмм">
+                        <img src={icon_2} alt='icon' />
+                    </a>
+                    <a href="#header" target="_blank" rel="noreferrer" className="contact_link" title="Напишите мне в ВК">
+                        <img src={icon_3} alt='icon' />
+                    </a>
+                    <a href="#header" target="_blank" rel="noreferrer" className="contact_link" title="На GIT можно посмотреть мои работы">
+                        <img src={icon_4} alt='icon' />
+                    </a>
+                </div>
+                <button className="btn_close" onClick={handleClose}>close</button>
+            </div>
+        </>
+    );
+}
