da69e1498333970057f190d1f4eccc037318e850
Leonid <leoural66@yandex.ru>
2020-12-29 20:40:02 +0500

    наполнение контентом

--- src/components/modalWindow.js
+++ src/components/modalWindow.js
@@ -18,16 +18,16 @@ export default function ModalWindow(props) {
                 <h3>Modal Window</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore odio facilis minus nisi obcaecati ipsum numquam sunt, magni sint omnis nobis accusamus harum, velit ratione.</p>
                 <div className="contact_jumbo_block_links">
-                    <a href="#header" target="_blank" rel="noreferrer" className="contact_link" title="Напишите мне в Инстаграмм">
+                    <a href="https://www.instagram.com/leo.photo_ural/" target="_blank" rel="noreferrer" className="contact_link" title="Напишите мне в Инстаграмм">
                         <img src={icon_1} alt='icon' />
                     </a>
-                    <a href="#header" target="_blank" rel="noreferrer" className="contact_link" title="Напишите мне в Телеграмм">
+                    <a href="https://t.me/LeoUral" target="_blank" rel="noreferrer" className="contact_link" title="Напишите мне в Телеграмм">
                         <img src={icon_2} alt='icon' />
                     </a>
-                    <a href="#header" target="_blank" rel="noreferrer" className="contact_link" title="Напишите мне в ВК">
+                    <a href="https://vk.com/leo_ural" target="_blank" rel="noreferrer" className="contact_link" title="Напишите мне в ВК">
                         <img src={icon_3} alt='icon' />
                     </a>
-                    <a href="#header" target="_blank" rel="noreferrer" className="contact_link" title="На GIT можно посмотреть мои работы">
+                    <a href="https://github.com/LeoUral" target="_blank" rel="noreferrer" className="contact_link" title="На GIT можно посмотреть мои работы">
                         <img src={icon_4} alt='icon' />
                     </a>
                 </div>
